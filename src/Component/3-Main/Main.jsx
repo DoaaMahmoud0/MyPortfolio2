fbeb7c6371cf4f1881ff222b37c34624d6321455
Doaa <doaamahmoud.dm2017@gmail.com>
2025-02-12 15:47:07 +0200

    commit

--- src/Component/3-Main/Main.jsx
+++ src/Component/3-Main/Main.jsx
@@ -11,6 +11,8 @@ const Main = () => {
       imgPath: "2.png",
       link: "https://myportfolio-livid-rho.vercel.app/",
       github: "https://github.com/DoaaMahmoud0/MyPortfolio2",
+      subTitle: "This project showcases a personal portfolio website where I present my web development skills and projects. The website is built using a combination of HTML, CSS, JavaScript, and React, aiming to provide a clean and interactive user experience.",
+
     },
     {
       projectTitle: "Portfolio",
@@ -18,6 +20,8 @@ const Main = () => {
       imgPath: "3.png",
       link: "my-portfolio2-navy.vercel.app",
       github: "https://github.com/DoaaMahmoud0/MyPortfolio2",
+      subTitle: "This project showcases a personal portfolio website where I present my web development skills and projects. The website is built using a combination of HTML, CSS, JavaScript, and React, aiming to provide a clean and interactive user experience.",
+
     },
     {
       projectTitle: "Restaurant Website",
@@ -25,6 +29,8 @@ const Main = () => {
       imgPath: "4.png",
       link: "https://doaamahmoud0.github.io/little-lemon/",
       github: "https://github.com/DoaaMahmoud0/little-lemon",
+      subTitle: "This project is a fully functional website designed for a restaurant, built using HTML, CSS, JavaScript, and React. The goal was to create an engaging, user-friendly platform where customers can explore the restaurant’s menu, view the ambiance, and easily make reservations"
+
     },
     {
       projectTitle: "Landing Page",
@@ -32,6 +38,8 @@ const Main = () => {
       imgPath: "PSD.png",
       link: "https://doaamahmoud0.github.io/LandingPage/",
       github: "https://github.com/DoaaMahmoud0/LandingPage",
+      subTitle: "The medical services landing page is designed for a smooth user experience, offering easy navigation for booking appointments and exploring services. Its clean, responsive layout ensures accessibility across all devices for optimal convenience."
+
     },
     {
       projectTitle: "Courses Website ",
@@ -39,41 +47,57 @@ const Main = () => {
       imgPath: "./imgs/9.png",
       link: "https://doaamahmoud0.github.io/Nouvil/",
       github: "https://github.com/DoaaMahmoud0/Nouvil",
+      subTitle:"This project is a fully functional Online Courses Website designed to provide users with an easy-to-navigate platform to browse, register, and learn from a wide variety of online courses. Built with HTML and CSS, the website offers a dynamic and engaging user experience for both students and instructors."
+
     },
     {
       projectTitle: "Coffe Website",
       category: "react",
       imgPath: "./imgs/1.jpg",
+      subTitle: "This project is a fully functional website , built using HTML, CSS, JavaScript, and React. The goal was to create an engaging, user-friendly platform where customers can explore the menu, view the ambiance, and easily make orders"
+
     },
     {
       projectTitle: "Shoes Website",
       category: "react",
       imgPath: "./imgs/3.jpg",
+      subTitle: "This project is a fully functional website , built using HTML, CSS, JavaScript, and React. The goal was to create an engaging, user-friendly platform where customers can explore the menu, view the ambiance, and easily make orders"
+
     },
     {
       projectTitle: "Cars Website",
       category: "react",
       imgPath: "./imgs/5.jpg",
+      subTitle: "This project is a fully functional website , built using HTML, CSS, JavaScript, and React. The goal was to create an engaging, user-friendly platform where customers can explore the menu, view the ambiance, and easily make orders"
+
     },
     {
       projectTitle: "Portfolio",
       category: "react",
       imgPath: "./imgs/6.jpg",
+      subTitle: "This project showcases a personal portfolio website where I present my web development skills and projects. The website is built using a combination of HTML, CSS, JavaScript, and React, aiming to provide a clean and interactive user experience.",
+
     },
     {
       projectTitle: "Furniture Website",
       category: "react",
       imgPath: "./imgs/7.jpg",
+      subTitle: "This project is a fully functional website , built using HTML, CSS, JavaScript, and React. The goal was to create an engaging, user-friendly platform where customers can explore the menu, view the ambiance, and easily make orders"
+
     },
     {
       projectTitle: "Landing Page",
       category: "react",
       imgPath: "./imgs/8.jpg",
+      subTitle: "This project is a fully functional Landing Page, built using HTML, CSS, JavaScript, and React. The goal was to create an engaging, user-friendly platform where customers can explore the menu, view the ambiance, and easily make orders"
+
     },
     {
       projectTitle: "Watches Website",
       category: "react",
       imgPath: "./imgs/b.png",
+      subTitle: "This project is a fully functional website , built using HTML, CSS, JavaScript, and React. The goal was to create an engaging, user-friendly platform where customers can explore the menu, view the ambiance, and easily make orders"
+
     },
     
   ];
@@ -134,11 +158,7 @@ const Main = () => {
                 <div style={{ width: "210px" }} className="box">
                   <h1 className="title">{items.projectTitle}</h1>
                   <p className="sub-title">
-                    The medical services landing page is designed for a smooth
-                    user experience, offering easy navigation for booking
-                    appointments and exploring services. Its clean, responsive
-                    layout ensures accessibility across all devices for optimal
-                    convenience.
+                   {items.subTitle}
                   </p>
 
                   <div className="icons flex">
