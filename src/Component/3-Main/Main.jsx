31b2add8630b301c22a36b38b4e2e806c52982b5
Doaa <doaamahmoud.dm2017@gmail.com>
2025-10-25 01:35:49 +0300

    Fix: open project links in new tab

--- src/Component/3-Main/Main.jsx
+++ src/Component/3-Main/Main.jsx
@@ -11,17 +11,17 @@ const Main = () => {
       imgPath: "2.png",
       link: "https://myportfolio-livid-rho.vercel.app/",
       github: "https://github.com/DoaaMahmoud0/MyPortfolio2",
-      subTitle: "This project showcases a personal portfolio website where I present my web development skills and projects. The website is built using a combination of HTML, CSS, JavaScript, and React, aiming to provide a clean and interactive user experience.",
-
+      subTitle:
+        "This project showcases a personal portfolio website where I present my web development skills and projects. The website is built using a combination of HTML, CSS, JavaScript, and React, aiming to provide a clean and interactive user experience.",
     },
     {
       projectTitle: "Portfolio",
       category: "react",
       imgPath: "3.png",
-      link: "my-portfolio2-navy.vercel.app",
+      link: "https://my-portfolio2-navy.vercel.app/",
       github: "https://github.com/DoaaMahmoud0/MyPortfolio2",
-      subTitle: "This project showcases a personal portfolio website where I present my web development skills and projects. The website is built using a combination of HTML, CSS, JavaScript, and React, aiming to provide a clean and interactive user experience.",
-
+      subTitle:
+        "This project showcases a personal portfolio website where I present my web development skills and projects. The website is built using a combination of HTML, CSS, JavaScript, and React, aiming to provide a clean and interactive user experience.",
     },
     {
       projectTitle: "Restaurant Website",
@@ -29,8 +29,8 @@ const Main = () => {
       imgPath: "4.png",
       link: "https://doaamahmoud0.github.io/little-lemon/",
       github: "https://github.com/DoaaMahmoud0/little-lemon",
-      subTitle: "This project is a fully functional website designed for a restaurant, built using HTML, CSS, JavaScript, and React. The goal was to create an engaging, user-friendly platform where customers can explore the restaurant’s menu, view the ambiance, and easily make reservations"
-
+      subTitle:
+        "This project is a fully functional website designed for a restaurant, built using HTML, CSS, JavaScript, and React. The goal was to create an engaging, user-friendly platform where customers can explore the restaurant’s menu, view the ambiance, and easily make reservations",
     },
     {
       projectTitle: "Landing Page",
@@ -38,8 +38,8 @@ const Main = () => {
       imgPath: "PSD.png",
       link: "https://doaamahmoud0.github.io/LandingPage/",
       github: "https://github.com/DoaaMahmoud0/LandingPage",
-      subTitle: "The medical services landing page is designed for a smooth user experience, offering easy navigation for booking appointments and exploring services. Its clean, responsive layout ensures accessibility across all devices for optimal convenience."
-
+      subTitle:
+        "The medical services landing page is designed for a smooth user experience, offering easy navigation for booking appointments and exploring services. Its clean, responsive layout ensures accessibility across all devices for optimal convenience.",
     },
     {
       projectTitle: "Courses Website ",
@@ -47,59 +47,58 @@ const Main = () => {
       imgPath: "./imgs/9.png",
       link: "https://doaamahmoud0.github.io/Nouvil/",
       github: "https://github.com/DoaaMahmoud0/Nouvil",
-      subTitle:"This project is a fully functional Online Courses Website designed to provide users with an easy-to-navigate platform to browse, register, and learn from a wide variety of online courses. Built with HTML and CSS, the website offers a dynamic and engaging user experience for both students and instructors."
-
+      subTitle:
+        "This project is a fully functional Online Courses Website designed to provide users with an easy-to-navigate platform to browse, register, and learn from a wide variety of online courses. Built with HTML and CSS, the website offers a dynamic and engaging user experience for both students and instructors.",
     },
     {
       projectTitle: "Coffe Website",
       category: "react",
       imgPath: "./imgs/1.jpg",
-      subTitle: "This project is a fully functional website , built using HTML, CSS, JavaScript, and React. The goal was to create an engaging, user-friendly platform where customers can explore the menu, view the ambiance, and easily make orders"
-
+      subTitle:
+        "This project is a fully functional website , built using HTML, CSS, JavaScript, and React. The goal was to create an engaging, user-friendly platform where customers can explore the menu, view the ambiance, and easily make orders",
     },
     {
       projectTitle: "Shoes Website",
       category: "react",
       imgPath: "./imgs/3.jpg",
-      subTitle: "This project is a fully functional website , built using HTML, CSS, JavaScript, and React. The goal was to create an engaging, user-friendly platform where customers can explore the menu, view the ambiance, and easily make orders"
-
+      subTitle:
+        "This project is a fully functional website , built using HTML, CSS, JavaScript, and React. The goal was to create an engaging, user-friendly platform where customers can explore the menu, view the ambiance, and easily make orders",
     },
     {
       projectTitle: "Cars Website",
       category: "react",
       imgPath: "./imgs/5.jpg",
-      subTitle: "This project is a fully functional website , built using HTML, CSS, JavaScript, and React. The goal was to create an engaging, user-friendly platform where customers can explore the menu, view the ambiance, and easily make orders"
-
+      subTitle:
+        "This project is a fully functional website , built using HTML, CSS, JavaScript, and React. The goal was to create an engaging, user-friendly platform where customers can explore the menu, view the ambiance, and easily make orders",
     },
     {
       projectTitle: "Portfolio",
       category: "react",
       imgPath: "./imgs/6.jpg",
-      subTitle: "This project showcases a personal portfolio website where I present my web development skills and projects. The website is built using a combination of HTML, CSS, JavaScript, and React, aiming to provide a clean and interactive user experience.",
-
+      subTitle:
+        "This project showcases a personal portfolio website where I present my web development skills and projects. The website is built using a combination of HTML, CSS, JavaScript, and React, aiming to provide a clean and interactive user experience.",
     },
     {
       projectTitle: "Furniture Website",
       category: "react",
       imgPath: "./imgs/7.jpg",
-      subTitle: "This project is a fully functional website , built using HTML, CSS, JavaScript, and React. The goal was to create an engaging, user-friendly platform where customers can explore the menu, view the ambiance, and easily make orders"
-
+      subTitle:
+        "This project is a fully functional website , built using HTML, CSS, JavaScript, and React. The goal was to create an engaging, user-friendly platform where customers can explore the menu, view the ambiance, and easily make orders",
     },
     {
       projectTitle: "Landing Page",
       category: "react",
       imgPath: "./imgs/8.jpg",
-      subTitle: "This project is a fully functional Landing Page, built using HTML, CSS, JavaScript, and React. The goal was to create an engaging, user-friendly platform where customers can explore the menu, view the ambiance, and easily make orders"
-
+      subTitle:
+        "This project is a fully functional Landing Page, built using HTML, CSS, JavaScript, and React. The goal was to create an engaging, user-friendly platform where customers can explore the menu, view the ambiance, and easily make orders",
     },
     {
       projectTitle: "Watches Website",
       category: "react",
       imgPath: "./imgs/b.png",
-      subTitle: "This project is a fully functional website , built using HTML, CSS, JavaScript, and React. The goal was to create an engaging, user-friendly platform where customers can explore the menu, view the ambiance, and easily make orders"
-
+      subTitle:
+        "This project is a fully functional website , built using HTML, CSS, JavaScript, and React. The goal was to create an engaging, user-friendly platform where customers can explore the menu, view the ambiance, and easily make orders",
     },
-    
   ];
   const [currentActive, setcurrentActive] = useState("all");
   const [arr, setArr] = useState(myprojects);
@@ -148,8 +147,8 @@ const Main = () => {
             return (
               <motion.article
                 layout
-                initial={{ transform: "scale:0" }}
-                animate={{ transform: "scale:1" }}
+                initial={{ scale: 0 }}
+                animate={{ scale: 1 }}
                 transition={{ type: "spring", damping: 8, stiffness: 50 }}
                 key={items.imgPath}
                 className="card">
@@ -157,9 +156,7 @@ const Main = () => {
 
                 <div style={{ width: "210px" }} className="box">
                   <h1 className="title">{items.projectTitle}</h1>
-                  <p className="sub-title">
-                   {items.subTitle}
-                  </p>
+                  <p className="sub-title">{items.subTitle}</p>
 
                   <div className="icons flex">
                     <div style={{ gap: "11px" }} className="flex">
@@ -178,11 +175,13 @@ const Main = () => {
                         <div className="icon-github"></div>
                       </a>
                     </div>
-                    <a className="link flex" href="">
+                    <a
+                      className="link flex"
+                      href={items.link}
+                      target="_blank"
+                      rel="noopener noreferrer">
                       More
-                      <span
-                        style={{ alignSelf: "end" }}
-                        className="icon-arrow-right"></span>
+                      <span className="icon-arrow-right"></span>
                     </a>
                   </div>
                 </div>
